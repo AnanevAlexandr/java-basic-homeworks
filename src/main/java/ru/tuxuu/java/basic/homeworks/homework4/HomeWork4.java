90ab983640152ebbca803954498ad26b179310b0
AnanevAlexandr <115997478+AnanevAlexandr@users.noreply.github.com>
2024-10-11 16:55:54 +0400

    ДЗ №4 на проверку

---
+++ src/main/java/ru/tuxuu/java/basic/homeworks/homework4/HomeWork4.java
@@ -0,0 +1,44 @@
+package ru.tuxuu.java.basic.homeworks.homework4;
+
+public class HomeWork4 {
+    public static void main(String[] args) {
+
+        User[] user = new User[10];
+        user[0] = new User("Аабов", "Ашот", "Арменович", 1986, "aabov@mail.ru");
+        user[1] = new User("Бобров>", "Дмитрий", "Александрович", 1964, "bobr@mail.ru");
+        user[2] = new User("Винокур", "Артем", "Дмитриевич", 1972, "vinokur@mail.ru");
+        user[3] = new User("Гришин", "Андрей", "Данилович", 1958, "grishin@mail.ru");
+        user[4] = new User("Донич", "Марк", "Ильич", 1994, "donich@mail.ru");
+        user[5] = new User("Ефимов", "Олег", "Вадимович", 1949, "efimof@mail.ru");
+        user[6] = new User("Жиров", "Константин", "Евгеньевич", 1980, "zhirov@mail.ru");
+        user[7] = new User("Зимин", "Илья", "Алексеевич", 1977, "zimin@mail.ru");
+        user[8] = new User("Иванов", "Иван", "Иванович", 1989, "ivanov@mail.ru");
+        user[9] = new User("Котов", "Вадим", "Анатольевич", 1971, "kotov@mail.ru");
+
+
+        for (int i = 0; i < user.length; i++) {
+            if (2024 - user[i].getYearOfBirth() > 40) {
+                System.out.println(user[i].getSurname() + " " + user[i].getName() +
+                        " " + user[i].getPatronymic() + " " + user[i].getYearOfBirth() +
+                        " " + user[i].getEmail());
+            }
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Задача с коробкой");
+
+
+        Box box1 = new Box(3, 3, 3, "Зеленая");
+        box1.info();
+        box1.setColor("Красная");
+        box1.info();
+        box1.open(true);
+        box1.input();
+        box1.open(false);
+        box1.output();
+        box1.open(true);
+        box1.output();
+    }
+}
+
