656ce7824ed1d5991347667fd300b429e1b3dfe0
AnanevAlexandr <115997478+AnanevAlexandr@users.noreply.github.com>
2024-10-16 17:43:27 +0400

    ДЗ №8 на проверку

---
+++ src/main/java/ru/tuxuu/java/basic/homeworks/homework8/AppArraySizeException.java
@@ -0,0 +1,22 @@
+package ru.tuxuu.java.basic.homeworks.homework8;
+
+public class AppArraySizeException extends RuntimeException {
+    public AppArraySizeException() {
+    }
+
+    public AppArraySizeException(String message) {
+        super(message);
+    }
+
+    public AppArraySizeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AppArraySizeException(Throwable cause) {
+        super(cause);
+    }
+
+    public AppArraySizeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
