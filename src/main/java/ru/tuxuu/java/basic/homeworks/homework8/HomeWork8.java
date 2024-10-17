b8b74d9d76fa41480ff372e34e48dc736418ce41
AnanevAlexandr <115997478+AnanevAlexandr@users.noreply.github.com>
2024-10-17 12:53:00 +0400

    Внесены изменения по упрощению и замене дублирования кода

--- src/main/java/ru/tuxuu/java/basic/homeworks/homework8/HomeWork8.java
+++ src/main/java/ru/tuxuu/java/basic/homeworks/homework8/HomeWork8.java
@@ -19,24 +19,25 @@ public class HomeWork8 {
 
     public static void mas(String[][] mas) {
         int sum = 0;
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = 0; j < mas[i].length; j++) {
-                if (mas.length != 4 || mas[i].length != 4) {
-                    System.out.println("Текущая длина массива " + mas.length + " на " + mas[i].length);
-                    throw new AppArraySizeException("Размер данного массива не 4х4 !");
-                }
+        if (mas.length != 4) {
+            throw new AppArraySizeException("Длина данного массива не 4х4!");
+        }
+        for (String[] ma : mas) {
+            if (ma.length != 4) {
+                throw new AppArraySizeException("Ширина данного массива не 4х4!");
             }
         }
         System.out.println("Передан корректный массив 4х4!");
 
         for (int i = 0; i < mas.length; i++) {
             for (int j = 0; j < mas[i].length; j++) {
+                int value;
                 try {
-                    Integer.parseInt(mas[i][j]);
+                    value = Integer.parseInt(mas[i][j]);
                 } catch (NumberFormatException e) {
                     throw new AppArrayDataException("Ячейка " + i + "-" + j + " содержит слово " + mas[i][j]);
                 }
-                sum += Integer.parseInt(mas[i][j]);
+                sum += value;
             }
         }
         System.out.println("Сумма всех преобразованных элементов массива " + sum);
