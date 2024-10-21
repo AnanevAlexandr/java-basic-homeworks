4cec20e28ceb5fa0fd6e79da9e2e7febd3545f9b
AnanevAlexandr <115997478+AnanevAlexandr@users.noreply.github.com>
2024-10-21 11:38:49 +0400

    Изменения по замечаниям

--- src/main/java/ru/tuxuu/java/basic/homeworks/homework10/HomeWork10.java
+++ src/main/java/ru/tuxuu/java/basic/homeworks/homework10/HomeWork10.java
@@ -2,15 +2,15 @@ package ru.tuxuu.java.basic.homeworks.homework10;
 
 public class HomeWork10 {
     public static void main(String[] args) {
-        PhoneBookUtils cont = new PhoneBookUtils();
-        cont.add("Дима","89654321897");
-        cont.add("Вася","8-987-654-32-10");
-        cont.add("Саша","номер");
-        cont.add("Саша","8964-889-28-47");
-        cont.add("Саша","номер 574437");
-        cont.add("Паша","89654321897");
-        cont.add("Леша","89654321897");
-        System.out.println(cont.getBook());
+        PhoneBook cont = new PhoneBook();
+        cont.add("Дима", "89654321897");
+        cont.add("Вася", "8-987-654-32-10");
+        cont.add("Саша", "номер");
+        cont.add("Саша", "8964-889-28-47");
+        cont.add("Саша", "номер 574437");
+        cont.add("Паша", "89654321897");
+        cont.add("Леша", "89654321897");
+        System.out.println(cont.getContact());
         System.out.println(cont.find("Дима"));
         cont.containsPhoneNumber("89654321897");
 
