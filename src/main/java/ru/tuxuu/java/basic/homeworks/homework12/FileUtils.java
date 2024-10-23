55519956e5292368eafbb298dbd31efc5506e36e
AnanevAlexandr <115997478+AnanevAlexandr@users.noreply.github.com>
2024-10-23 17:03:17 +0400

    ДЗ №12 на проверку

---
+++ src/main/java/ru/tuxuu/java/basic/homeworks/homework12/FileUtils.java
@@ -0,0 +1,46 @@
+package ru.tuxuu.java.basic.homeworks.homework12;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class FileUtils {
+
+
+    public static void filesInFolder(String path) {
+        System.out.println("Найденные файлы: ");
+        File dirFolder = new File(path);
+        for (File fileEntry : dirFolder.listFiles()) {
+            if (fileEntry.getName().lastIndexOf("txt") > 0) {
+                System.out.println(fileEntry.getName());
+            }
+        }
+    }
+
+    public static void readFromFile(String file) throws FileNotFoundException {
+        System.out.print("Содержимое файла: ");
+        try (InputStreamReader in = new InputStreamReader(
+                new BufferedInputStream(
+                        new FileInputStream(file)))) {
+            int n = in.read();
+            while (n > 0) {
+                System.out.print((char) n);
+                n = in.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeInFile(String text, String path)  {
+        try (OutputStreamWriter out = new OutputStreamWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(path, true)))) {
+            byte[] buffer = text.getBytes(StandardCharsets.UTF_8);
+            for (int i = 0; i < buffer.length; i++) {
+                out.write(buffer[i]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
